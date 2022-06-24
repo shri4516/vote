55e8d43dcf95171b6f599846f6373fe436f171c8
shri4516 <yuvashri1312@gmail.com>
2022-06-24 09:39:26 +0530

    Done

---
+++ src/vote/org/Voting.java
@@ -0,0 +1,14 @@
+package vote.org;
+
+public class Voting {
+	public static void main(String[] args) {
+		int age=10;
+		if(age>=18) {
+		System.out.println("ELGIBLE TO VOTE");
+		}
+		else {
+			System.out.println("NOT ELGIBLE TO VOTE");
+		}
+	}
+
+}
